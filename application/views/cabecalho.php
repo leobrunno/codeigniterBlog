02a029e96e036903a2721b517d64c392208874b3
leobrunno <leobrunno@live.com>
2020-07-11 16:22:59 -0300

    Modificações na navbar

--- application/views/cabecalho.php
+++ application/views/cabecalho.php
@@ -1,8 +1,10 @@
 <html>
     <div id = "cabecalho">
-    <link href = "http://localhost/codeigniterBlog/assets/css/estilo.css" rel = "stylesheet"/>
-        <h2><?= anchor('home/index', "Blog do Fulano de tal");?></h2>
-        
+    <link href="https://getbootstrap.com/docs/3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
         <?php if ($this->session->flashdata('error') == TRUE): ?>
             <h2><?=$this->session->flashdata('error')?></h2>
         <?php endif; ?>
@@ -11,25 +13,22 @@
             <h2><?=$this->session->flashdata('success')?></h2>
         <?php endif; ?>
 
-        <div id = "formbusca">
-            <div id = "formbusca">
-                <?= form_open(base_url().'home/buscar'); ?>
-                <?= form_label("Buscar", "busca"); ?>
-                <?php $data = array('name' => 'busca', 'id' => 'busca', 'class' => 'form-control mr-sm-2'); ?>
-                <?= form_input($data); ?>
-                <?= form_submit('btn btn-outline-success my-2 my-sm-0', 'Buscar'); ?>
-                <?= form_close(); ?>
-            </div>
+          <a class="navbar-brand" href="<?=base_url();?>">Blog do fulano de tal</a>
         </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form action="<?=base_url()?>home/buscar" id="formbusca" class="navbar-form navbar-right" method="post">
+            <div class="form-group">
+              <input type="input" id="busca" name="busca" placeholder="buscar" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Buscar</button>
+            <?= anchor('usuario/logout', 'Sair');?>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
         <div>
             <?= anchor('usuario/logout', 'Sair');?>
         </div>
     </div>
     <br class = 'clear-both'>
 </html>
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
