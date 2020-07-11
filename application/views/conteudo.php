6220ad66161a130c53c9fc92752a5ad7467c6aec
leobrunno <leobrunno@live.com>
2020-07-11 16:54:26 -0300

    Novo layout de conteudo

--- application/views/conteudo.php
+++ application/views/conteudo.php
@@ -1,20 +1,26 @@
 <div id = "conteudo">
-<link href = "http://localhost/codeigniterBlog/assets/css/estilo.css" rel = "stylesheet"/>
-    <h2> Conteudo das postagens </h2>
-    <div id = 'grid-home'>
-        <?php foreach($postagens as $posts):?>
-            <div class = 'item-grid-home'>
-                <h2>
-                    <?= $posts['titulo']; ?>
-                </h2>
-                <h3>
-                    <?= $posts['conteudo']; ?>
-                </h3>
-                <h3>
-                    <?=anchor('home/editar/'.$posts['id'],'Editar')."</br>"?>
-                    <?=anchor('home/excluir/'.$posts['id'],'Excluir')?>
-                </h3>
-            </div>
-        <?php endforeach;?>
+<!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<link href="https://getbootstrap.com/docs/3.3/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="https://getbootstrap.com/docs/3.3/examples/jumbotron/jumbotron.css" rel="stylesheet">
+
+<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="https://getbootstrap.com/docs/3.3/assets/js/ie-emulation-modes-warning.js"></script>
+<div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+	  <?php foreach ($postagens as $post):?>
+        <div class="col-md-4">
+          <h2><?= $post['titulo'];?></h2>
+          <p><?= $post['conteudo'];?> </p>
+          <?=anchor('home/editar/'.$post['id'],'Editar')."</br>"?>
+          <?=anchor('home/excluir/'.$post['id'],'Excluir')?>
+        </div>
+		<?php endforeach;?>
     </div>
 </div>
